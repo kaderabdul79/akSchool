c7705be050509df582db537cedb66c6f52f6c837
kaderabdul22 <mostafakamal9300@gmail.com>
2021-10-19 10:07:24 +0600

    react router implemented

--- src/App.js
+++ src/App.js
@@ -4,14 +4,40 @@ import User from './components/Users/User';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <User></User>
+      {/* <User></User>
       <About></About>
       <Home></Home>
-      <NotFound></NotFound>
+      <NotFound></NotFound> */}
+
+      <Router>
+        <Switch>
+            <Route path="/home">
+                <Home></Home>
+            </Route>
+            <Route exact path="/">
+                <Home></Home>
+            </Route>
+            <Route path="/about">
+                <About></About>
+            </Route>
+            <Route path="/user">
+                <User></User>
+            </Route>
+            <Route path="*">
+                <NotFound></NotFound>
+            </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
